1160e19e9b8e56870c05c784081eac7a7a847c5d
yangjj <1542103461@qq.com>
2020-01-10 13:28:11 +0800

    feat: 更新配置

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ function resolve (dir) {
 module.exports = {
     lintOnSave: false,
     productionSourceMap: false,
-    publicPath: process.env.NODE_ENV === 'production' ? '/base/' : '//localhost:9083/',
+    publicPath: process.env.NODE_ENV === 'production' ? '//localhost:9003/' : '//localhost:9083/',
     configureWebpack: {
         externals: {
             'vue': 'Vue',
